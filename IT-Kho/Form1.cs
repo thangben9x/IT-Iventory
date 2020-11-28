f70de1cbe66f817dea32aa789f609decf0c1e189
nguyensonthach2010 <69343583+nguyensonthach2010@users.noreply.github.com>
2020-11-28 16:51:11 +0700

    ab

--- IT-Kho/Form1.cs
+++ IT-Kho/Form1.cs
@@ -16,7 +16,7 @@ namespace IT_Kho
         {
             InitializeComponent();
         }
-
+        //
         private void accordionControlElement1_Click(object sender, EventArgs e)
         {
 
